5cd319965c6a93cfdadea68b70b19c790ee34052
EvanGIT88 <evanfersadi2008@gmail.com>
2026-04-07 17:52:24 +0700

    Used function and direct address value assign

--- main.c
+++ main.c
@@ -2,6 +2,78 @@
 #include <stdlib.h>
 #include <conio.h>
 
+  /*
+    Display mechanism:
+    1. Idenfity 1d array as 2d using h and w var (matrix)
+    2. Convert x,y input to identified 2d array index
+    3. Convert width and height to identified 2d index array corresponding to the x,y
+  */
+int* create_arr_int(int n)
+{
+    int* arr = (int*)(malloc(n * sizeof(int)));
+    return arr;
+}
+
+bool setup(int* player_coord_addr, int* canvas_size_addr, int* virtual_coord_addr, char* matrix_arr_addr) {
+   player_coord_addr = create_arr_int(2);
+   canvas_size_addr = create_arr_int(2);
+   virtual_coord_addr = create_arr_int(2);
+
+   if (player_coord_addr == NULL || canvas_size_addr == NULL || virtual_coord_addr == NULL ) {
+     return false;
+   }
+
+   *player_coord_addr[0] = 1;
+   *player_coord_addr[1] = 1;
+   
+   *canvas_size_addr[0] = 32;
+   *canvas_size_addr[1] = 16;
+
+   *virtual_coord_addr[0] = 0;
+   *virtual_coord_addr[1] = 0;
+
+   matrix_arr_addr = malloc(canvas_size_addr[0] * canvas_size_addr[1] * sizeof(char));
+   return true;
+}
+
+void render(int* canvas_size_addr, char* matrix_arr_addr, int* virtual_coord_addr, int* player_coord_addr) {
+  for (int i = 0; i < (canvas_size_addr[0] * canvas_size_addr[1]); i++) {
+      matrix_arr_addr[i] = ' ';
+      if (i >= canvas_size_addr[0]) {
+        virtual_coord_addr[1] = virtual_coord_addr[1] + 1;
+        virtual_coord_addr[0] = 0;
+      }
+
+      if (virtual_coord_addr[0] == player_coord_addr[0] && virtual_coord_addr[1] == player_coord_addr[1]) {
+          matrix_arr_addr[i] = '|';
+      }
+      
+      virtual_coord_addr[0] = virtual_coord_addr[0] + 1;
+  }
+        
+}
+
+int main () {
+  int player_coord;   //(x, y)
+  int canvas_size;   //(w, h)
+  int virtual_coord; //(x, y)
+  char matrix_arr;
+
+  if(!setup(&player_coord, &canvas_size, &virtual_coord, &matrix_arr)) {
+    printf("Alloc failed!");
+    getchar();
+    return 1;
+  }
+
+  render(&canvas_size, &matrix_arr, &virtual_coord, &player_coord);
+
+  getchar();
+
+  return 0;
+}
+
+
+  /*
 int main() {
     //keypress input
     int ch;
@@ -52,12 +124,6 @@ int main() {
     pad_size_ptr[0] = 1; //width
     pad_size_ptr[1] = 5; //height
 
-    /*
-     1. Idenfity 1d array as 2d using h and w var (matrix)
-     2. Convert x,y input to identified 2d array index
-     3. Convert width and height to identified 2d index array corresponding to the x,y
-    */
-
     while ((ch = getch()) != '#'){
         for (int i = 0; i < canvas_h * canvas_w; i++) {
           if (i >= canvas_w) {
@@ -78,3 +144,4 @@ int main() {
 
     return 0;
 }
+*/
---
+++ refs/another_test.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+int main() {
+
+    while(1){
+    if (getch()) {
+       printf("%d", getch());
+    }
+    }
+
+    getchar();
+    
+    return 0;
+}
